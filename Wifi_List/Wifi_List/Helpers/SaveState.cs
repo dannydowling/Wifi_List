608264297fb380eabd9dc7fd21d2ccc4d49c1052
Danny Dowling <danny.dowling@gmail.com>
2021-10-19 17:42:18 -0800

    thought of a bug, naming an ssid the same name as the block list

--- Wifi_List/Wifi_List/Helpers/SaveState.cs
+++ Wifi_List/Wifi_List/Helpers/SaveState.cs
@@ -21,16 +21,41 @@ namespace Wifi_List.Helpers
         //remove one Network by name from preferences
         internal List<string> clearBlockedNetwork(string key)
         {
+            //make a new place to store the list of blocked networks
             List<string> blockedList = new List<string>();
-            string addedBlock = Preferences.Get("blockedList", "");
 
+            //bring down the list as one big horrible string
+            //ssid's are allowed to be 32 bytes long. 
+            //So we count to 33 to avoid that bug of naming an ssid as the list name.
+            string addedBlock = Preferences.Get("123456789101112131415161718192021222324252627282930313233", "");
+
+            //if it's not empty
             if (addedBlock != null)
             {
+                //split it up into columns
                 var columns = addedBlock.Split(',');
-                blockedList.Add(columns[0]);
+
+                //go over the columns, adding to the list.
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    blockedList.Add(i.ToString());
+                }
+
+                //remove the key from the blocked list
+                blockedList.Remove(key);
+
+                //re-write the preference file
+                foreach (var item in blockedList)
+                {
+                    Preferences.Set("123456789101112131415161718192021222324252627282930313233", item.ToString());
+                }
+               
             }            
 
+            //outside of the list of network names only, keyed by "blockedList", retrieve the actual blocked network
             retrieveBlockedNetwork(key, "");
+
+            //and clear that from preferences
             Preferences.Clear(key);
 
             return blockedList;
